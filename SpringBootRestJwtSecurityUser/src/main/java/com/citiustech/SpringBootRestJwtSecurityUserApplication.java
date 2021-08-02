0596d6dc92313134bb53333054a594c679d8e756
UshaBangaru <ushabangaru26@gmail.com>
2021-08-02 13:42:25 +0530

    Added Spring Security JWt

---
+++ SpringBootRestJwtSecurityUser/src/main/java/com/citiustech/SpringBootRestJwtSecurityUserApplication.java
@@ -0,0 +1,13 @@
+package com.citiustech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJwtSecurityUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJwtSecurityUserApplication.class, args);
+	}
+
+}
