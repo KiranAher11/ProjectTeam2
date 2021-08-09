be4bbce87755534b56a11f6c388b289b923883f3
Kiran Aher <KiranA3@citiustech.com>
2021-08-09 20:34:27 +0530

    small change in emailcontroller

--- SpringBootRestJwtSecurityUser/src/main/java/com/citiustech/restcontroller/EmailController.java
+++ SpringBootRestJwtSecurityUser/src/main/java/com/citiustech/restcontroller/EmailController.java
@@ -1,6 +1,9 @@
 package com.citiustech.restcontroller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +25,21 @@ public class EmailController {
 	@Autowired
 	EmailSenderService service;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	@PostMapping("/send/{email}")
 	public String sendPasswordToUser(@PathVariable String email) {
 		
-		boolean flag = userRepository.existsByEmail(email); 
-		
+		boolean flag = userRepository.existsByEmail(email);
+				
 		if(flag) {
 		String message = "Your new password is : password@123";
 		String subject = "Mail from CT Genaeral Hospital";
 		service.sendSimpleEmail(email , message, subject);
+		
+		Optional<User> user = userRepository.findByEmail(email);
+		user.ifPresent(e -> e.setPassword(encoder.encode("password@123")));
 		return "Email sent successfully";
 		}		
 		else {
