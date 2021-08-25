1b11df15685e7c965f07e4022eb109ce9a50ee09
Kiran Aher <KiranA3@citiustech.com>
2021-08-25 19:05:19 +0530

    change in role util class

--- springboot-backend/SpringBootRestJwtSecurityUser/src/main/java/com/citiustech/util/RolesUtils.java
+++ springboot-backend/SpringBootRestJwtSecurityUser/src/main/java/com/citiustech/util/RolesUtils.java
@@ -11,27 +11,37 @@ import com.citiustech.repo.RoleRepository;
 
 @Component
 public class RolesUtils {
-	
+
 	@Autowired
 	private RoleRepository repository;
-	
+
 	public void mapRoles(String usrRoles, Set<Role> dbRoles) {
-		if(usrRoles == null || usrRoles.isEmpty()) {
-			Role userRole = repository.findByName(ERole.ROLE_PATIENT).orElseThrow(()->new RuntimeException("Error : Role is not found"));
-			dbRoles.add(userRole);		
-			}else {
+	
+			switch (usrRoles) {
+			
+			case "ROLE_ADMIN":
+				Role role1 = repository.findByName(ERole.ROLE_ADMIN)
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+				dbRoles.add(role1);
+				break;
 				
-					switch(usrRoles) {
-					case "admin":
-						Role adminRole = repository.findByName(ERole.ROLE_ADMIN).orElseThrow(()->new RuntimeException("Error: Role is not found"));
-						dbRoles.add(adminRole);
-						break;
-						
-						default:
-							Role userRole = repository.findByName(ERole.ROLE_PATIENT).orElseThrow(()->new RuntimeException("Error: Role is not found"));
-							dbRoles.add(userRole);
-							break;
-						}
-			}
+			case "ROLE_PHYSICIAN":
+				Role role2 = repository.findByName(ERole.ROLE_PHYSICIAN)
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+				dbRoles.add(role2);
+				break;
+			case "ROLE_NURSE":
+				Role role3 = repository.findByName(ERole.ROLE_NURSE)
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+				dbRoles.add(role3);
+				break;
+				
+			case "ROLE_PATIENT":
+				Role role4 = repository.findByName(ERole.ROLE_PATIENT)
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+				dbRoles.add(role4);
+				break;
+			
+		}
 	}
 }
